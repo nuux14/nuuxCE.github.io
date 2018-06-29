8b349096d0bb2c6496b13c59447c7cd0cbba795b
nuux14 <nuux14@gmail.com>
2018-06-29 04:58:58 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register('./sw.js').then(reg => {
 
         if (reg.installing) {
             console.log('Service worker installing');
